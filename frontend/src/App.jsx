3a915bdc1c02e989cde8af189845196583521cf8
chintan <imchintan0207@gmail.com>
2025-05-16 00:04:37 +0530

    feat:  add Problem page with AdminRoute and authUser check

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,8 @@ import { Toaster } from "react-hot-toast";
 import { useAuthStore } from "./store/useAuthStore";
 import { Loader } from "lucide-react";
 import Layout from "./layout/Layout";
+import AddProblem from "./Pages/AddProblem";
+import AdminRoute from "./components/AdminRoute";
 
 const App = () => {
   const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
@@ -43,6 +45,13 @@ const App = () => {
           path="/login"
           element={!authUser ? <LoginPage /> : <Navigate to={"/"} />}
         />
+
+        <Route element={<AdminRoute />}>
+          <Route
+            path="/add-problem"
+            element={authUser ? <AddProblem /> : <Navigate to={"/"} />}
+          />
+        </Route>
       </Routes>
     </div>
   );
